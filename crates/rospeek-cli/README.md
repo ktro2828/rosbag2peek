a97b9b8ddc31fe205c90777a9e140cb4a0008c4e
ktro2828 <kotaro.uetake@tier4.jp>
2025-08-07 14:08:50 +0900

    feat: update to sort topics by name
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>

--- crates/rospeek-cli/README.md
+++ crates/rospeek-cli/README.md
@@ -4,22 +4,33 @@ A crate for the command line interface for `rospeek`.
 
 ## Usage
 
+```shell
+cargo install --path crates/rospeek-cli
+```
+
 ### List Topics
 
 ```shell
-cargo run rospeek-cli -- -b <BAG_FILE.db3> list-topics
+rospeek list <BAG_FILE>
 ```
 
 ```shell
+  Topic name  |  [Message type]  |  (Serialization format)
 - /test_topic [std_msgs/String] (cdr)
 ```
 
 ### Show a Specific Topic
 
 ```shell
-cargo run rospeek-cli -- -b <BAG_FILE.db3> show --topic <TOPIC_NAME> --count <NUM_COUNT>
+rospeek show <BAG_FILE> --topic <TOPIC_NAME> --count <NUM_COUNT>
 ```
 
 ```shell
 [0] t = 1234567890 ns, 7 bytes
+```
+
+### Decode CDR-encoded messages into JSON
+
+```shell
+rosppek decode <BAG_FILE> --topic <TOPIC_NAME>
 ```
--- crates/rospeek-cli/src/main.rs
+++ crates/rospeek-cli/src/main.rs
@@ -1,7 +1,7 @@
 use clap::{Parser, Subcommand};
 use rospeek_core::{BagReader, CdrDecoder, MessageSchema, RosPeekError, RosPeekResult};
 use rospeek_db3::Db3Reader;
-use std::path::PathBuf;
+use std::{collections::BTreeMap, path::PathBuf};
 
 #[derive(Parser)]
 #[command(name = "rospeek", about = "Peek into rosbag files", long_about = None)]
@@ -12,8 +12,8 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Commands {
-    /// List all topics int the bag
-    ListTopics {
+    /// List all topics in the bag file
+    List {
         #[arg(value_name = "BAGFILE", help = "Path to the .db3 bag file")]
         bag: PathBuf,
     },
@@ -26,11 +26,11 @@ enum Commands {
         #[arg(short, long, help = "Topic name to read messages (e.g. /tf)")]
         topic: String,
 
-        #[arg(short, long, default_value_t = 1, help = "Number of messages to show")]
-        count: usize,
+        #[arg(short, long, help = "Number of messages to show")]
+        count: Option<usize>,
     },
 
-    /// Decode CDR to json
+    /// Decode CDR-encoded messages into JSON
     Decode {
         #[arg(value_name = "BAGFILE", help = "Path to the .db3 bag file")]
         bag: PathBuf,
@@ -44,16 +44,30 @@ fn main() -> RosPeekResult<()> {
     let cli = Cli::parse();
 
     match cli.command {
-        Commands::ListTopics { bag } => {
+        Commands::List { bag } => {
             // TODO(ktro2828): add support of McapReader
             let reader: Box<dyn BagReader> = Box::new(Db3Reader::open(bag)?);
 
-            let topics = reader.topics()?;
-            for topic in topics {
-                println!(
-                    "- {} [{}] ({})",
-                    topic.name, topic.type_name, topic.serialization_format
-                );
+            let mut grouped: BTreeMap<String, Vec<_>> = BTreeMap::new();
+            for topic in reader.topics()? {
+                let key = topic
+                    .name
+                    .split("/")
+                    .filter(|s| !s.is_empty())
+                    .next()
+                    .map(|s| format!("/{}", s))
+                    .unwrap_or_else(|| "/".to_string());
+                grouped.entry(key).or_default().push(topic);
+            }
+            println!("  Topic name  |  [Message type]  |  (Serialization format)");
+            for (_, mut topics) in grouped {
+                topics.sort_by(|a, b| a.name.cmp(&b.name));
+                for topic in topics {
+                    println!(
+                        "- {} [{}] ({})",
+                        topic.name, topic.type_name, topic.serialization_format
+                    );
+                }
             }
         }
         Commands::Show { bag, topic, count } => {
@@ -61,7 +75,8 @@ fn main() -> RosPeekResult<()> {
             let reader: Box<dyn BagReader> = Box::new(Db3Reader::open(bag)?);
 
             let messages = reader.read_messages(&topic)?;
-            for (i, msg) in messages.iter().take(count).enumerate() {
+            let n = count.unwrap_or(messages.len());
+            for (i, msg) in messages.iter().take(n).enumerate() {
                 println!("[{}] t = {} ns, {} bytes", i, msg.timestamp, msg.data.len());
             }
         }
