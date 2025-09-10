c7b00ac2e198602acd9fe3d2156a1014f2953438
ktro2828 <kotaro.uetake@tier4.jp>
2025-09-10 15:45:37 +0900

    feat: update McapReader
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>

--- crates/rospeek-mcap/src/reader.rs
+++ crates/rospeek-mcap/src/reader.rs
@@ -12,14 +12,38 @@ pub struct McapReader {
     stats: BagStats,
 }
 
+impl McapReader {
+    fn into_stream(&self) -> RosPeekResult<MessageStream<'_>> {
+        MessageStream::new(&self.mmap)
+            .map_err(|e| RosPeekError::Other(format!("Failed to create message stream: {}", e)))
+    }
+}
+
 impl BagReader for McapReader {
     fn open<P: AsRef<Path>>(path: P) -> RosPeekResult<Self> {
         let fd = File::open(path.as_ref())?;
         let mmap = unsafe { Mmap::map(&fd) }?;
 
         // TODO(kto2828): Implement stats calculation
-        let start_ns = i64::MAX;
-        let end_ns = i64::MIN;
+        let mut start_ns = i64::MAX;
+        let mut end_ns = i64::MIN;
+
+        let stream = MessageStream::new(&mmap)
+            .map_err(|e| RosPeekError::Other(format!("Failed to create message stream: {}", e)))?;
+
+        for message_result in stream.into_iter() {
+            let message = message_result
+                .map_err(|e| RosPeekError::Other(format!("Failed to read message: {}", e)))?;
+
+            let log_time = message.log_time as i64;
+
+            if log_time < start_ns {
+                start_ns = log_time;
+            }
+            if log_time > end_ns {
+                end_ns = log_time;
+            }
+        }
 
         let stats = BagStats {
             path: path.as_ref().display().to_string(),
@@ -38,28 +62,63 @@ impl BagReader for McapReader {
     }
 
     fn topics(&self) -> RosPeekResult<Vec<Topic>> {
-        for message in
-            MessageStream::new(&self.mmap).map_err(|_| RosPeekError::Other("".to_string()))?
-        {
-            println!(
-                "{:?}",
-                message.map_err(|_| RosPeekError::Other("".to_string()))?
+        use std::collections::HashMap;
+
+        let stream = self.into_stream()?;
+
+        let topic_map: Result<HashMap<String, Topic>, RosPeekError> = stream
+            .map(|message_result| {
+                message_result
+                    .map_err(|_| RosPeekError::Other("Failed to read message".to_string()))
+            })
+            .try_fold(
+                HashMap::<String, Topic>::new(),
+                |mut acc, message_result| {
+                    let message = message_result?;
+                    let topic_name = message.channel.topic.clone();
+
+                    acc.entry(topic_name.clone())
+                        .and_modify(|topic| topic.count += 1)
+                        .or_insert_with(|| Topic {
+                            id: message.channel.id as i64,
+                            name: topic_name,
+                            type_name: message
+                                .channel
+                                .schema
+                                .as_ref()
+                                .map(|s| s.name.clone())
+                                .unwrap_or_default(),
+                            count: 1,
+                            serialization_format: message.channel.message_encoding.clone(),
+                            offered_qos_profiles: None,
+                        });
+
+                    Ok::<HashMap<String, Topic>, RosPeekError>(acc)
+                },
             );
-        }
-        Ok(vec![])
+
+        topic_map.map(|map| map.into_values().collect())
     }
 
     fn read_messages(&self, topic_name: &str) -> RosPeekResult<Vec<RawMessage>> {
-        println!("Topic: {}", topic_name);
+        let stream = self.into_stream()?;
 
-        for message in
-            MessageStream::new(&self.mmap).map_err(|_| RosPeekError::Other("".to_string()))?
-        {
-            println!(
-                "{:?}",
-                message.map_err(|_| RosPeekError::Other("".to_string()))?
-            );
-        }
-        Ok(vec![])
+        let raw_messages = stream
+            .map(|message_result| {
+                message_result
+                    .map_err(|_| RosPeekError::Other("Failed to read message".to_string()))
+            })
+            .filter_map(|message_result| match message_result {
+                Ok(message) if message.channel.topic == topic_name => Some(Ok(RawMessage {
+                    timestamp: message.publish_time as i64,
+                    topic_id: message.channel.id as i64,
+                    data: message.data.into(),
+                })),
+                Ok(_) => None,          // Skip messages from other topics
+                Err(e) => Some(Err(e)), // Propagate errors
+            })
+            .collect::<RosPeekResult<Vec<RawMessage>>>();
+
+        raw_messages
     }
 }
